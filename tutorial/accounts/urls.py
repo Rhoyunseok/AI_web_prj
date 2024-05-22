46531d4abea3392de1cc17c832739d96f0633bcf
Rhoyunseok <74577789+Rhoyunseok@users.noreply.github.com>
2024-05-22 11:12:22 +0900

    signup & login function complete

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('signup/', signup, name='signup'),
+    path('home/signup/', signup, name='signup'),
+    path('home/login/', login, name='login'),
+    path('home/logout/', logout, name='logout'),
+    path('home/', home, name='home'),
 ]
--- tutorial/accounts/views.py
+++ tutorial/accounts/views.py
@@ -15,3 +15,23 @@ def signup(request):
             return redirect('/')
         return render(request, 'signup.html')
     return render(request, 'signup.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home') # return redirect('board')
+        else:
+            return render(request, 'login.html', {'error': 'username or password is incorrect.'})
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
+
+def home(request):
+    return render(request, 'home.html')
