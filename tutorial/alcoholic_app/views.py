031032a258bf21a4e3103a0cd34e4cec614c7a82
Rhoyunseok <74577789+Rhoyunseok@users.noreply.github.com>
2024-06-10 14:13:56 +0900

    # 삭제

--- tutorial/alcoholic_app/views.py
+++ tutorial/alcoholic_app/views.py
@@ -1,5 +1,4 @@
 # accounts/views.py
-import csv
 import logging
 import pandas as pd
 from django.contrib import auth
@@ -13,9 +12,9 @@ def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
             user = User.objects.create_user(
-                                            username=request.POST['username'],
-                                            password=request.POST['password1'],
-                                            email=request.POST['email'],)
+                                        username=request.POST['username'],
+                                        password=request.POST['password1'],
+                                        email=request.POST['email'],)
             auth.login(request, user)
             return redirect('/main/home/')
         return render(request, 'templates/signup.html', {'error': 'password must match.'})
