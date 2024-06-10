7a2b45faa2126b231d5a3bfac0581ea023e1f6ca
achahc <134582936+achahc@users.noreply.github.com>
2024-06-10 17:05:47 +0900

    beer,traditional_liquor,cocktail,wine 지도

---
+++ tutorial/beermap/beer.js
@@ -0,0 +1,40 @@
+window.initMap = function () {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 36.62813149999999, lng: 127.4563596 },
+        zoom: 18,
+    });
+
+
+    const malls = [
+        { label: "B", name: "무지개맥주충북대정문점", lat: 36.6352554, lng: 127.4386333 },
+        { label: "B", name: "역전할머니맥주 충북대점", lat: 36.6327273026081, lng:  127.458046292656},
+        { label: "B", name: "홀리데이펍", lat: 36.6321727131295, lng: 127.457374960333 },
+        { label: "B", name: "맥주창고", lat: 36.6327372706902 , lng: 127.458031816571 },
+        { label: "B", name: "가장맛있는맥주집", lat: 36.6225235221169, lng: 127.446703145266 },
+        { label: "B", name: "폼프리츠",lat: 36.6321743279182, lng: 127.457963301046 },
+        { label: "B", name: "크라운호츠 충북대", lat: 36.6331743499364, lng: 127.459264302026 },
+    ];
+
+    const bounds = new google.maps.LatLngBounds();
+    const infowindow = new google.maps.InfoWindow();
+
+
+    malls.forEach(({ label, name, lat, lng }) => {
+        const marker = new google.maps.Marker({
+            position: { lat, lng },
+            label,
+            map,
+        });
+        bounds.extend(marker.position);
+
+        marker.addListener("click", () => {
+            map.panTo(marker.position);// 지도 중심으로 이동
+            infowindow.setContent(name);//마커 클릭하면 상세정보 name 뜬다
+            infowindow.open({
+                anchor: marker,
+                map,
+            });
+        });
+    });
+
+};
---
+++ tutorial/cocktail/cocktail.js
@@ -0,0 +1,41 @@
+window.initMap = function () {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 36.62813149999999, lng: 127.4563596 },
+        zoom: 18,
+    });
+
+
+    const malls = [
+        { label: "C", name: "RnD", lat: 36.6328032376479, lng: 127.458382167373 },
+        { label: "C", name: "D&D (디앤디)", lat: 36.631131900581, lng: 127.460698056873 },
+        { label: "C", name: "이올렛", lat: 36.6339933028659, lng: 127.459986759708 },
+        { label: "C", name: "사막의 하얀꽃", lat: 36.632947811583, lng: 127.458958838156 },
+        { label: "C", name: "KUSH", lat: 36.6331936301859, lng: 127.458937934206 },
+        { label: "C", name: "190바 Bar", lat: 36.6394316, lng: 127.4329238 },
+        { label: "C", name: "아지트바", lat: 36.6394316, lng: 127.4329238 },
+
+    ];
+
+    const bounds = new google.maps.LatLngBounds();
+    const infowindow = new google.maps.InfoWindow();
+
+
+    malls.forEach(({ label, name, lat, lng }) => {
+        const marker = new google.maps.Marker({
+            position: { lat, lng },
+            label,
+            map,
+        });
+        bounds.extend(marker.position);
+
+        marker.addListener("click", () => {
+            map.panTo(marker.position);// 지도 중심으로 이동
+            infowindow.setContent(name);//마커 클릭하면 상세정보 name 뜬다
+            infowindow.open({
+                anchor: marker,
+                map,
+            });
+        });
+    });
+
+};
