2364d6a67902695071c4398436587a4b64752d5e
chanho99 <chanho0526@naver.com>
2024-06-10 12:30:28 +0900

    cocktail html/py 임시

--- tutorial/alcoholic_app/urls.py
+++ tutorial/alcoholic_app/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path('home/beer/', category_beer, name='category_beer'),
     path('home/beer/<str:category>/', beer_list, name='beer_list'),
     path('home/beer/list/<int:beer_index>', views.csv_view_pd, name='csv_view_pd'), #dddd
+    path('home/cocktail/', category_cocktail, name='category_cocktail'),
+    path('home/cocktail/<str:category>/', cocktail_list, name='cocktail_list'),
+    path('home/cocktail/list/<int:cocktail_index>', views.csv_view_cocktail, name='csv_view_cocktail'),
 ]
--- tutorial/alcoholic_app/views.py
+++ tutorial/alcoholic_app/views.py
@@ -78,6 +78,37 @@ def csv_view_pd(request, beer_index):
     
     return render(request, 'templates/beer_detail.html', context)
 
+def csv_view_cocktail(request, cocktail_index):
+    file_path = 'alcoholic_app/data/cocktail.csv'
+    
+    # pandas를 사용하여 CSV 파일을 읽습니다.
+    df = pd.read_csv(file_path, encoding='euc-kr', index_col= 0)
+    
+    # 특정 맥주 인덱스의 데이터를 가져옵니다.
+    if cocktail_index < len(df):
+        cocktail_data = df.iloc[cocktail_index]
+        context = {
+            'specific_data': {
+                'cocktail_name': cocktail_data.iloc[1],
+                'cocktail_img_url': cocktail_data.iloc[2],
+                'cocktail_category': cocktail_data.iloc[5],
+                'cocktail_index': cocktail_data.iloc[24],
+                'cocktail_alcohol': cocktail_data.iloc[0],
+                'cocktail_making': cocktail_data.iloc[4],
+                'cocktail_base_amount': cocktail_data.iloc[6],
+                'cocktail_liquor': cocktail_data.iloc[7],
+                'cocktail_liquor_amout': cocktail_data.iloc[8],
+                'cocktail_taste': cocktail_data.iloc[16],
+                'cocktail_type_glass': cocktail_data.iloc[17],
+            }
+        }
+    else:
+        context = {
+            'error': '해당 인덱스의 칵테일을 찾을 수 없습니다.'
+        }
+    
+    return render(request, 'templates/cocktail_detail.html', context)
+
 
 def category_beer(request):
     file_path = 'alcoholic_app/data/beer2.csv'
@@ -88,8 +119,6 @@ def category_beer(request):
 
 
 def beer_list(request, category):
-    import pandas as pd
-
     file_path = 'alcoholic_app/data/beer2.csv'
     df = pd.read_csv(file_path, encoding='euc-kr', index_col=0)
 
@@ -111,3 +140,35 @@ def beer_list(request, category):
         'page_obj': page_obj
     }
     return render(request, 'templates/beers_by_category.html', context)
+
+
+def category_cocktail(request):
+    file_path = 'alcoholic_app/data/cocktail.csv'
+    df = pd.read_csv(file_path, encoding='euc-kr', index_col= 0) # CSV 파일을 읽어 데이터프레임으로 변환
+
+    categories = df.iloc[:, 5].unique()  # iloc[5] 열의 유일한 카테고리 목록 가져오기
+    return render(request, 'templates/cocktail_list_test.html', {'categories': categories})
+
+
+def cocktail_list(request, category):
+    file_path = 'alcoholic_app/data/cocktail.csv'
+    df = pd.read_csv(file_path, encoding='euc-kr', index_col=0)
+
+    filtered_df = df[df.iloc[:, 5] == category].sort_values(by=df.columns[0])
+    cocktail_list = filtered_df.to_dict(orient='records')
+
+    for i, cocktail in enumerate(cocktail_list):
+        cocktail['cocktail_name'] = cocktail[df.columns[1]]  
+        cocktail['cocktail_img_url'] = cocktail[df.columns[2]] 
+        cocktail['cocktail_index'] = cocktail[df.columns[24]]
+        
+
+    paginator = Paginator(beer_list, 6)  
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'category': category,
+        'page_obj': page_obj
+    }
+    return render(request, 'templates/cocktail_by_category.html', context)
